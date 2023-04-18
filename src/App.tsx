097ecfedf5d0f520cf25f5b492cdb6fb1d9dde9e
bjornole-kodehode <bjornole.kodehode@gmail.com>
2023-04-18 22:49:32 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,108 @@
+import { useEffect, useState } from 'react'
+
+import './App.css'
+import { getData } from './api'
+const URL = "https://rickandmortyapi.com/api/character?species="
+type CharacterType = {
+  name: string,
+  image: string,
+  gender: string,
+  species: string,
+  cost: Cost
+}
+
+type SelectedType = "cost" | "name"
+
+enum Cost {
+  one = 1,
+  two = 2,
+  three = 3,
+  four = 4,
+  five = 5,
+  six = 6
+}
+
+function getCost(value: any): Cost {
+  switch (value) {
+    case "Human":
+      return Cost.one;
+    case "Alien":
+      return Cost.two;
+    case "unknown":
+      return Cost.three;
+    case "Animal":
+      return Cost.four;
+    case "Robot":
+      return Cost.five;
+    case "Mythological Creature":
+      return Cost.six
+    default:
+      return Cost.one
+  }
+}
+
+
+function App() {
+
+  const [characterData, setCharacterData] = useState<CharacterType[]>([])
+  const [selectedSorting, setSelectedSorting] = useState<SelectedType>("cost")
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+      const species = ["Human", "Alien", "Unknown", "Animal", "Robot", "Mythological"]
+      const promises = species.map(specie => getData(`${URL}${specie}`))
+      const results = await Promise.all(promises)
+      console.log("results", results)
+      
+      const datas = results.flatMap(data => data.results)
+      const characters: CharacterType[] = datas.map(({name, image, species}) => {
+        console.log("data: ", name, image, species)
+        return ({
+          name,
+          image,
+          species,
+          cost: getCost(species),
+          gender: ""
+      })
+      })  
+        setCharacterData(characters)
+      } catch (error) {
+        console.error(error)
+      }
+      
+    }
+    fetchData()
+  }, [])
+
+  const sortedByName = [...characterData].sort((a, b) => a.name.localeCompare(b.name))
+  const sortedByCost = [...characterData].sort((a, b) => a.cost - b.cost)
+  let sortedCharacters = characterData
+  if (selectedSorting === "cost") {
+    sortedCharacters = sortedByCost
+  } else if (selectedSorting === "name") {
+    sortedCharacters = sortedByName
+  }
+  console.log("sortedByName", sortedByName)
+  console.log("characterData: ", characterData)
+  return (
+    <div className="App">
+      <button className='py-2 px-4 hover:bg-blue-400 bg-blue-500 rounded my-6' onClick={() => setSelectedSorting(prevState => prevState === "cost" ? "name" : "cost")}>name/cost</button>
+      <div className='grid gap-2 grid-cols-7'>
+        {sortedCharacters.map(({name, image, cost, gender, species}) => (
+          <div className=''>
+            <img src={image} />
+            <div className='flex flex-col'>
+              <span>{name}</span>
+              <span>{gender}</span>
+              <span>{species}</span>
+              <span>{cost}</span>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/api/index.ts
@@ -0,0 +1,8 @@
+import axios from "axios"
+
+export async function getData(url: string, species?: string){
+    const params = species ? { species } : {}
+    const response = await axios.get(url, { params })
+    
+    return response.data
+}
