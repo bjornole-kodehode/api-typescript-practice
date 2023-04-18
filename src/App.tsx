5c17cf8662b48d57a9b11112d5c0dc177c375340
bjornole-kodehode <bjornole.kodehode@gmail.com>
2023-04-19 00:03:14 +0200

    better formating, adding border w dynamical color

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from "react";
 
-import './App.css'
-import { getData } from './api'
-import { CharacterType, Cost, SelectedType } from './types';
-const URL = "https://rickandmortyapi.com/api/character?species="
+import "./App.css";
+import { getData } from "./api";
+import { CharacterType, Cost, SelectedType } from "./types";
+const URL = "https://rickandmortyapi.com/api/character?species=";
 
 function getCost(value: any): Cost {
   switch (value) {
@@ -18,63 +18,95 @@ function getCost(value: any): Cost {
     case "Robot":
       return Cost.five;
     case "Mythological Creature":
-      return Cost.six
+      return Cost.six;
     default:
-      return Cost.one
+      return Cost.one;
   }
 }
 
-
 function App() {
-
-  const [characterData, setCharacterData] = useState<CharacterType[]>([])
-  const [selectedSorting, setSelectedSorting] = useState<SelectedType>("cost")
+  const [characterData, setCharacterData] = useState<CharacterType[]>([]);
+  const [selectedSorting, setSelectedSorting] = useState<SelectedType>("cost");
 
   useEffect(() => {
     async function fetchData() {
       try {
-      const species = ["Human", "Alien", "Unknown", "Animal", "Robot", "Mythological"]
-      const promises = species.map(specie => getData(`${URL}${specie}`))
-      const results = await Promise.all(promises)
-      
-      
-      const datas = results.flatMap(data => data.results)
-      const characters: CharacterType[] = datas.map(({name, image, species}) => {
-        
-        return ({
-          name,
-          image,
-          species,
-          cost: getCost(species),
-          gender: ""
-      })
-      })  
-        setCharacterData(characters)
-      } catch (error) {
-        console.error(error)
-      }
-      
-    }
-    fetchData()
-  }, [])
+        const species = [
+          "Human",
+          "Alien",
+          "Unknown",
+          "Animal",
+          "Robot",
+          "Mythological",
+        ];
+        const promises = species.map((specie) => getData(`${URL}${specie}`));
+        const results = await Promise.all(promises);
 
-  const sortedByName = [...characterData].sort((a, b) => a.name.localeCompare(b.name))
-  const sortedByCost = [...characterData].sort((a, b) => a.cost - b.cost)
-  let sortedCharacters = characterData
+        const returnedData = results.flatMap((data) => data.results);
+        const characters: CharacterType[] = returnedData.map(
+          ({ name, image, species }) => {
+            return {
+              name,
+              image,
+              species,
+              cost: getCost(species),
+              gender: "",
+            };
+          }
+        );
+        setCharacterData(characters);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    fetchData();
+  }, []);
+
+  const sortedByName = [...characterData].sort((a, b) =>
+    a.name.localeCompare(b.name)
+  );
+  const sortedByCost = [...characterData].sort((a, b) => a.cost - b.cost);
+  let sortedCharacters = characterData;
   if (selectedSorting === "cost") {
-    sortedCharacters = sortedByCost
+    sortedCharacters = sortedByCost;
   } else if (selectedSorting === "name") {
-    sortedCharacters = sortedByName
+    sortedCharacters = sortedByName;
   }
-  
+
   return (
     <div className="App">
-      <button className='py-2 px-4 hover:bg-blue-400 bg-blue-500 rounded my-6' onClick={() => setSelectedSorting(prevState => prevState === "cost" ? "name" : "cost")}>name/cost</button>
-      <div className='grid gap-2 grid-cols-7'>
-        {sortedCharacters.map(({name, image, cost, gender, species}) => (
-          <div className=''>
-            <img src={image} />
-            <div className='flex flex-col'>
+      <button
+        className="py-2 px-4 hover:bg-blue-400 bg-blue-500 rounded my-6"
+        onClick={() =>
+          setSelectedSorting((prevState) =>
+            prevState === "cost" ? "name" : "cost"
+          )
+        }
+      >
+        name/cost
+      </button>
+      <div className="grid gap-2 grid-cols-7">
+        {sortedCharacters.map(({ name, image, cost, gender, species }) => (
+          <div className="">
+            <img
+              className={`rounded-full p-1 border-4 ${
+                cost === 1
+                  ? "border-gray-500"
+                  : cost === 2
+                  ? "border-green-500"
+                  : cost === 3
+                  ? "border-blue-500"
+                  : cost === 4
+                  ? "border-purple-500"
+                  : cost === 5
+                  ? "border-orange-500"
+                  : cost === 6
+                  ? "border-cyan-500"
+                  : ""
+              }`}
+              src={image}
+            />
+            <div className="flex flex-col">
               <span>{name}</span>
               <span>{gender}</span>
               <span>{species}</span>
@@ -84,7 +116,7 @@ function App() {
         ))}
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
